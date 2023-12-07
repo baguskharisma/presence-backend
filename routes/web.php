7ee33ec53b9a1df3b064e835063dfe5899da554f
baguskharismaaa <baguskharisma.work@gmail.com>
2023-12-07 14:16:25 +0700

    latest update

--- routes/web.php
+++ routes/web.php
@@ -26,24 +26,31 @@ Route::get('/', [HomeController::class, 'index'])->name('home');
 
 Route::get('/admin', [AdminController::class, 'index'])->middleware('is_admin')->name('admin');
 Route::get('/admin/create-user', [AdminController::class, 'createUser'])->middleware('is_admin')->name('create-user');
-Route::get('/admin/position', [AdminController::class, 'position'])->middleware('is_admin')->name('position');
-Route::get('/admin/department', [AdminController::class, 'department'])->middleware('is_admin')->name('department');
-Route::get('/admin/create-position', [AdminController::class, 'createPosition'])->middleware('is_admin')->name('create-position');
-Route::get('/admin/create-department', [AdminController::class, 'createDepartment'])->middleware('is_admin')->name('create-department');
-Route::get('/admin/edit-user/{id}', [AdminController::class, 'editUser'])->middleware('is_admin')->name('edit-user');
-Route::get('/admin/manage-presence', [AdminController::class, 'managePresence'])->middleware('is_admin')->name('manage-presence');
-Route::get('/admin/create-schedule', [ScheduleController::class, 'createschedule']);
 Route::post('/admin/create-user', [AdminController::class, 'storeUser']);
-Route::post('/admin/create-position', [AdminController::class, 'storePosition']);
-Route::post('/admin/create-department', [AdminController::class, 'storeDepartment']);
-Route::post('/admin/create-schedule', [ScheduleController::class, 'storeSchedule'])->name('create-schedule');
+Route::get('/admin/edit-user/{id}', [AdminController::class, 'editUser'])->middleware('is_admin')->name('edit-user');
 Route::put('/admin/{id}', [AdminController::class, 'updateUser'])->name('update-user');
 Route::delete('/admin/user/{id}', [AdminController::class, 'deleteUser'])->name('delete-user');
+
+Route::get('/admin/position', [AdminController::class, 'position'])->middleware('is_admin')->name('position');
+Route::get('/admin/create-position', [AdminController::class, 'createPosition'])->middleware('is_admin')->name('create-position');
+Route::post('/admin/create-position', [AdminController::class, 'storePosition']);
 Route::delete('/admin/position/{id}', [AdminController::class, 'deletePosition'])->name('delete-position');
+
+Route::get('/admin/department', [AdminController::class, 'department'])->middleware('is_admin')->name('department');
+Route::get('/admin/create-department', [AdminController::class, 'createDepartment'])->middleware('is_admin')->name('create-department');
+Route::post('/admin/create-department', [AdminController::class, 'storeDepartment']);
 Route::delete('/admin/department/{id}', [AdminController::class, 'deleteDepartment'])->name('delete-department');
 
 Route::get('/schedule', [ScheduleController::class, 'index'])->name('schedule');
 Route::get('/schedule/detail/{id}', [ScheduleController::class, 'scheduleDetail'])->name('schedule-detail');
+Route::get('/admin/create-schedule', [ScheduleController::class, 'createschedule']);
+Route::post('/admin/create-schedule', [ScheduleController::class, 'storeSchedule'])->name('create-schedule');
+
+Route::post('/presence/check-in', [PresenceController::class, 'checkIn'])->middleware('checkInGPS')->name('check-in');
+Route::post('/presence/check-out', [PresenceController::class, 'checkOut'])->middleware('checkOutGPS')->name('check-out');
+
+Route::get('/presence', [PresenceController::class, 'index'])->middleware('auth')->name('presence');
+Route::get('/admin/manage-presence', [AdminController::class, 'managePresence'])->middleware('is_admin')->name('manage-presence');
 
 Route::get('/permission/manage', [PermissionController::class, 'index'])->middleware('is_admin')->name('manage-permission');
 Route::get('/permission/create-permission', [PermissionController::class, 'createPermission'])->name('create-permission');
@@ -52,19 +59,8 @@ Route::post('/permission/manage/{id}', [PermissionController::class, 'manage']);
 
 Route::get('/login', [LoginController::class, 'index'])->middleware('guest')->name('login');
 Route::post('/login', [LoginController::class, 'authenticate']);
-
-Route::get('/presence', [PresenceController::class, 'index'])->middleware('auth')->name('presence');
+Route::post('/logout', [LoginController::class, 'logout']);
 
 Route::get('/history', [HistoryController::class, 'history'])->name('history');
 
 Route::get('/profil', [ProfileController::class, 'index'])->name('profile');
-
-Route::post('/logout', [LoginController::class, 'logout']);
-
-Route::post('/presence/check-in', [PresenceController::class, 'checkIn'])->middleware('checkInGPS')->name('check-in');
-Route::post('/presence/check-out', [PresenceController::class, 'checkOut'])->middleware('checkOutGPS')->name('check-out');
-
-
-
-
-
